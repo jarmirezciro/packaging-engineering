9b6a5bf0b6c79c962391a11c53c3818439670dd7
Alejandro Ramirez <jaramirezciro@gmail.com>
2026-06-16 15:34:25 +0200

    Configure persistent media serving for Railway soft launch

--- packagingproject/urls.py
+++ packagingproject/urls.py
@@ -1,31 +1,28 @@
-"""
-URL configuration for packagingproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
+"""Root URL configuration for the Packaging Engineering application."""
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.static import serve as serve_media
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('packagingapp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("packagingapp.urls")),
+]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# The application creates runtime engineering images and stores uploaded
+# catalogue files under MEDIA_ROOT. During the Railway soft launch, serve those
+# files from the same Django service and a persistent Railway Volume.
+#
+# This is intentionally controlled by SERVE_MEDIA_FILES so it can be disabled
+# later when media is moved to object storage or a dedicated media service.
+if settings.SERVE_MEDIA_FILES:
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve_media,
+            {"document_root": settings.MEDIA_ROOT},
+        ),
+    ]
